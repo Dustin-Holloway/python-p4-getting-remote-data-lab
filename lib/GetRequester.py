8f752780cbd556adb73c36af5cd3ed2941f6d4d2
Dustin <dustinhollowaypdx@gmail.com>
2023-05-23 19:19:47 -0700

    finished mini lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,30 @@
 import requests
 import json
 
-class GetRequester:
 
+class GetRequester:
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        url = self.url
+
+        response = requests.get(url)
+        return response.content
 
     def load_json(self):
-        pass
+        list = []
+        list_items = json.loads(self.get_response_body())
+        for thing in list_items:
+            list.append(thing)
+
+        return list
+
+
+requester = GetRequester(
+    url="https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+)
+
+thing = requester.load_json()
+
+print(thing)
